a217af11570a3ace85309fc16d1088b8d36c612b
Gabriel Rauter <rauter.gabriel@gmail.com>
2015-09-06 16:56:09 +0200

    remove debug message

--- sbh-custom.plugin.zsh
+++ sbh-custom.plugin.zsh
@@ -2,7 +2,6 @@ dir=$(dirname $0)
 config=(os-check aliases docker go less llvm ls)
 ((OSX)) && config=($config homebrew java-osx locale)
 for c in $config; do
-	echo ${dir}/${c}.zsh
 	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh";
 done
 
