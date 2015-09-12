535dd385a1d77e9419ffc2a393d03aed475d3af9
Gabriel Rauter <rauter.gabriel@gmail.com>
2015-09-12 18:41:10 +0200

    cleanup

--- my-conf.plugin.zsh
+++ my-conf.plugin.zsh
@@ -1,8 +1,8 @@
-[[ -z $CONFIG_PATH ]] && CONFIG_PATH="$(dirname $0)/config"
-dir=$CONFIG_PATH
+[[ -z $MY_CONF_PATH ]] && CONFIG_PATH="$(dirname $0)/config"
+dir=$MY_CONF_PATH
 source "$(dirname $0)/os-check.zsh"
 [[ -z $config && -f "${dir}/config.zsh" ]] && source "${dir}/config.zsh"
-for c in $config; do
+for c in $my-conf; do
 	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh"
 	[[ (OSX) && -f ${dir}/osx/${c}.zsh ]] && source "${dir}/osx/${c}.zsh"
 	[[ (LINUX) && -f ${dir}/linux/${c}.zsh ]] && source "${dir}/linux/${c}.zsh"
