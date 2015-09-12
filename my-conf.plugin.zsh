72eaf6c2a599f365301fff6e9acda6895336b639
Gabriel Rauter <rauter.gabriel@gmail.com>
2015-09-12 17:57:11 +0200

    auto source config.zsh if config files are not set before plugin call

--- my-conf.plugin.zsh
+++ my-conf.plugin.zsh
@@ -1,6 +1,7 @@
 [[ -z $CONFIG_PATH ]] && CONFIG_PATH="$(dirname $0)/config"
 dir=$CONFIG_PATH
 source "$(dirname $0)/os-check.zsh"
+[[ -z $config && -f "${dir}/config.zsh" ]] && source "${dir}/config.zsh"
 for c in $config; do
 	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh"
 	[[ (OSX) && -f ${dir}/osx/${c}.zsh ]] && source "${dir}/osx/${c}.zsh"
