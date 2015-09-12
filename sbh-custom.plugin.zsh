7d4147e59c9fd19482735e5e6250331261959bbe
Gabriel Rauter <rauter.gabriel@gmail.com>
2015-09-12 16:28:06 +0200

    rework how to include os specific configs

--- sbh-custom.plugin.zsh
+++ sbh-custom.plugin.zsh
@@ -1,8 +1,13 @@
-dir=$(dirname $0)
-config=(os-check aliases docker go less ls)
-((OSX)) && config=($config homebrew java-osx llvm-osx locale)
-((OSX)) && config=($config llvm-linux)
+[[ -z $CONFIG_PATH ]] && CONFIG_PATH=$(dirname $0)
+dir=$CONFIG_PATH
+config=(os-check aliases docker go less llvm ls)
+((OSX)) && config=($config homebrew java locale)
+((LINUX)) && config=($config)
+((MSYS)) && config=($config)
 for c in $config; do
-	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh";
+	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh"
+	[[ (OSX) && -f ${dir}/osx/${c}.zsh ]] && source "${dir}/osx/${c}.zsh"
+	[[ (LINUX) && -f ${dir}/linux/${c}.zsh ]] && source "${dir}/linux/${c}.zsh"
+	[[ (MSYS) && -f ${dir}/msys/${c}.zsh ]] && source "${dir}/msys/${c}.zsh"
 done
 
