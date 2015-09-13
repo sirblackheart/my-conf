23c2a2fe30ea0a38914cf77bd5e3519c69f26478
Gabriel Rauter <rauter.gabriel@gmail.com>
2015-09-13 03:12:22 +0200

    bugfix

--- my-conf.plugin.zsh
+++ my-conf.plugin.zsh
@@ -3,8 +3,8 @@ dir=$MY_CONF_PATH
 source "$(dirname $0)/os-check.zsh"
 [[ -z $config && -f "${dir}/config.zsh" ]] && set -A myconf && source "${dir}/config.zsh"
 for c in $myconf; do
-	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh"
-	[[ (OSX) && -f ${dir}/osx/${c}.zsh ]] && source "${dir}/osx/${c}.zsh"
-	[[ (LINUX) && -f ${dir}/linux/${c}.zsh ]] && source "${dir}/linux/${c}.zsh"
-	[[ (MSYS) && -f ${dir}/msys/${c}.zsh ]] && source "${dir}/msys/${c}.zsh"
+	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh";
+	((OSX)) && [[ -f ${dir}/osx/${c}.zsh ]] && source "${dir}/osx/${c}.zsh";
+	(LINUX) &&[[ -f ${dir}/linux/${c}.zsh ]] && source "${dir}/linux/${c}.zsh";
+	(MSYS) && [[-f ${dir}/msys/${c}.zsh ]] && source "${dir}/msys/${c}.zsh";
 done
