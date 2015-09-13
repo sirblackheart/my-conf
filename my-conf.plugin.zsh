194b060f08040f9396f6a3e2aadf73148265acb8
Gabriel Rauter <rauter.gabriel@gmail.com>
2015-09-14 01:15:40 +0200

    missing space

--- my-conf.plugin.zsh
+++ my-conf.plugin.zsh
@@ -6,5 +6,5 @@ for c in $myconf; do
 	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh";
 	((OSX)) && [[ -f ${dir}/osx/${c}.zsh ]] && source "${dir}/osx/${c}.zsh";
 	((LINUX)) &&[[ -f ${dir}/linux/${c}.zsh ]] && source "${dir}/linux/${c}.zsh";
-	((MSYS)) && [[-f ${dir}/msys/${c}.zsh ]] && source "${dir}/msys/${c}.zsh";
+	((MSYS)) && [[ -f ${dir}/msys/${c}.zsh ]] && source "${dir}/msys/${c}.zsh";
 done
