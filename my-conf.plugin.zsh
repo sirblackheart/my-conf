47a696f9c9e51a8674fe3e994f6bbf9638efc081
Gabriel Rauter <rauter.gabriel@gmail.com>
2016-03-13 00:07:30 +0100

    set an os name usable as string

--- my-conf.plugin.zsh
+++ my-conf.plugin.zsh
@@ -4,7 +4,5 @@ source "$(dirname $0)/os-check.zsh"
 [[ -z $config && -f "${dir}/config.zsh" ]] && set -A myconf && source "${dir}/config.zsh"
 for c in $myconf; do
 	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh";
-	((OSX)) && [[ -f ${dir}/osx/${c}.zsh ]] && source "${dir}/osx/${c}.zsh";
-	((LINUX)) &&[[ -f ${dir}/linux/${c}.zsh ]] && source "${dir}/linux/${c}.zsh";
-	((MSYS)) && [[ -f ${dir}/msys/${c}.zsh ]] && source "${dir}/msys/${c}.zsh";
+	[[ -f ${dir}/$OS_NAME/${c}.zsh ]] && source "${dir}/$OS_NAME/${c}.zsh";
 done
--- os-check.zsh
+++ os-check.zsh
@@ -4,8 +4,11 @@ export OSX=0
 export MSYS=0
 if [[ "$unamestr" == "Linux" ]]; then
 	export LINUX=1
+	export OS_NAME="linux"
 elif [[ "$unamestr" == "Darwin" ]]; then
 	export OSX=1
+	export OS_NAME="osx"
 elif [[ "$unamestr" == "MSYS"* ]]; then
 	export MSYS=1
+	export OS_NAME="msys"
 fi
