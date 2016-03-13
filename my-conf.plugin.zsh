880f6f8c132f1bd5ee916d51da4403a20b0746c2
Gabriel Rauter <rauter.gabriel@gmail.com>
2016-03-13 15:58:27 +0100

    add new load() function to load zsh files from my conf folder directly

--- my-conf.plugin.zsh
+++ my-conf.plugin.zsh
@@ -6,3 +6,15 @@ for c in $myconf; do
 	[[ -f ${dir}/${c}.zsh ]] && source "${dir}/${c}.zsh";
 	[[ -f ${dir}/$OS_NAME/${c}.zsh ]] && source "${dir}/$OS_NAME/${c}.zsh";
 done
+load() {
+	if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
+		echo "my-conf: name [os] and script to load"
+	fi
+    if [ "$#" -eq 1 ]; then
+		[[ -f ${dir}/$1.zsh ]] && source "${dir}/$1.zsh";
+		[[ -f ${dir}/$OS_NAME/$1.zsh ]] && source "${dir}/$OS_NAME/$1.zsh";
+    fi
+    if [ "$#" -eq 2 ]; then
+		[[ -f ${dir}/$1/$2.zsh ]] && source "${dir}/$1/$2.zsh";
+    fi
+}
